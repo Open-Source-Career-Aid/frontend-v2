a153015e38255d305067536354279b72077e3f67
preciousonah <preciousonah12@gmail.com>
2024-04-24 15:48:58 -0500

    add dumblevel component with tailwind css styling

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const APIURL='https://api.dumbsplain.com';
+export const avatarlabels = ['Just Plain Dumb', 'Not Too Bright', 'Smartass', 'Pretentious Professor', 'Sentient Savant']
+export const GTAG = 'G-5F2TKF5294'
