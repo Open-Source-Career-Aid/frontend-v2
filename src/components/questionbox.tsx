6a625ae2f4dd69e93a00964a34d616e849244788
Chinmay Shrivastava <chinmay.shrivastava.1999@gmail.com>
2024-05-04 10:45:46 -0400

    questionbox init

--- src/components/questionbox.tsx
+++ src/components/questionbox.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+
 export default function QuestionBox() {
+    const questionBoxHeader = React.useRef<HTMLDivElement>(null);
     return (
-        <div className="border border-1 border-black flex-grow mx-32 rounded-lg">
-            <h1>QuestionBox</h1>
+        <div className="border border-1 border-black flex-grow mx-32 rounded-lg overflow-hidden h-full">
+            <div className="border border-b-1 bg-gray-200 w-full" ref={questionBoxHeader}>
+                <span>Topic of the day:</span>
+                <span className="font-bold">General Knowledge</span>
+            </div>
+            <div className="overflow-y-scroll p-4"
+                style={{ height: `calc(100% - ${questionBoxHeader.current?.clientHeight}px)` }}
+            >
+                <span>Question:</span>
+                <span className="font-bold">What is the capital of France?</span>
+            </div>
         </div>
     )
 }
