55453dee642cf3feb5bf95c5614a998c2d799710
Chinmay Shrivastava <chinmay.shrivastava.1999@gmail.com>
2024-05-04 10:19:48 -0400

    modal

--- src/components/infomodal.tsx
+++ src/components/infomodal.tsx
@@ -3,10 +3,13 @@ import BasicModal from "./modalbox"
 export default function InfoModal() {
     return (
         <div>
-            <button>
-                <img src="info.png" alt="dark mode" className='h-10' />
-            </button>
-            <BasicModal />
+            <BasicModal
+                buttonchildren={<img src="info.png" alt="info" className='h-10' />}
+            >
+                <h1>Instructions</h1>
+                <p>Click on the tiles to reveal the hidden image. The goal is to find all the matching pairs in the shortest time possible.</p>
+                <p>Good luck!</p>
+            </BasicModal>
         </div>
     )
 }
--- src/components/modalbox.tsx
+++ src/components/modalbox.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
@@ -11,32 +10,28 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
   p: 4,
 };
 
-export default function BasicModal() {
+export default function BasicModal({ buttonchildren, children }: { buttonchildren: React.ReactNode, children: React.ReactNode }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <button onClick={handleOpen}>
+        {buttonchildren}
+      </button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        className="flex justify-center items-center rounded-md drop-shadow-lg"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Box sx={style} className='rounded-md dark:bg-gray-800 dark:text-white w-[400px] h-[600px]'>
+         {children}
         </Box>
       </Modal>
     </div>
