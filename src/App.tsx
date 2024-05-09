c2b896465748263c52da9e79b6ad3072ec9d4411
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-05-09 17:56:58 -0400

    remove: remove old components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import DumbLevels from './components/dumblevels';
 import Navbar from './components/navbar';
-import QuestionBox from './components/questionbox';
 
 function App() {
   return (
-    <div className="App flex flex-col justify-between h-screen px-32 dark:bg-gray-800">
+    <div className="App flex flex-col justify-between h-screen px-32 dark:bg-gray-800 max-w-[826px] mx-auto">
       <Navbar />
       <div className='flex flex-col h-full mx-32'>
-        <QuestionBox />
-        <DumbLevels />
       </div>
     </div>
   );
