da8d7658c759ca9e44f5d94e14932fe78cfc7847
Chinmay Shrivastava <chinmay.shrivastava.1999@gmail.com>
2024-05-09 14:27:33 -0400

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-// --dark-gray: 0 0 0 0.9;
-
-//     --text-primary: 224 84 18 1;
-//     --text-secondary: 224 26 47 1;
-
-//     --card-outlining-dark: 223 22 75 1;
-//     --disabled-buttons: 223 22 75 1;
-
-//     --card-outlining-light: 223 21 87 1;
-//     --unselected-option: 223 21 87 1;
-//     --page-break: 223 21 87 1;
     extend: {
       colors: {
-        'dark-gray': 'rgba(--dark-gray)',
-        'text-primary': 'rgba(--text-primary)',
-        'text-secondary': 'rgba(--text-secondary)',
-        'card-outlining-dark': 'rgba(--card-outlining-dark)',
-        'disabled-buttons': 'rgba(--disabled-buttons)',
-        'card-outlining-light': 'rgba(--card-outlining-light)',
-        'unselected-option': 'rgba(--unselected-option)',
-        'page-break': 'rgba(--page-break)',
+        
+        'dark-gray': 'rgba(24, 27, 35, 1)',
+        'text-primary': 'rgba(7, 27, 83, 1)',
+        'text-secondary': 'rgba(88, 105, 149, 1)',
+        'card-outlining-strong': 'rgba(176, 184, 205, 1)',
+        'disabled-buttons': 'rgba(176, 184, 205, 1)',
+        'card-outlining-lite': 'rgba(216, 220, 230, 1)',
+        'unselected-option': 'rgba(216, 220, 230, 1)',
+        'page-break': 'rgba(216, 220, 230, 1)',
+        'bg-primary': 'rgba(246, 249, 255, 1)',
+
+        'blue-strong': 'rgba(19, 78, 242, 1)',
+        'blue-primary': 'rgba(76, 123, 254, 1)',
+        'blue-hover': 'rgba(132, 163, 250, 1)',
+        'blue-selected': 'rgba(194, 209, 255, 1)',
+        'blue-illustration': 'rgba(230, 236, 255, 1)',
+
+        'orange-strong': 'rgba(250, 142, 43, 1)',
+
+        'green-strong': 'rgba(104, 189, 138, 1)',
+        'green-lite': 'rgba(169, 232, 171, 1)',
+
+        'red-strong': 'rgba(248, 38, 38, 1)',
+        'red-lite': 'rgba(249, 138, 138, 1)'
+      
       },
     },
   },
