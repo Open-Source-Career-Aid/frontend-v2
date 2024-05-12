49377983810311d2a7676b083406fc760876621f
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-05-11 22:00:30 -0400

    add: framer and base motion

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -5,6 +5,7 @@ import { TopicHeader } from "./headers/topicheader";
 import Image from "./image";
 import StickyButton from "./stickybutton";
 import Footer from "./footer";
+import { motion } from "framer-motion";
 
 export default function Base() {
     return (
@@ -15,13 +16,27 @@ export default function Base() {
                 <div className="flex justify-center w-full mt-6">
                     <Logo showfull />
                 </div>
-                <BotMessage>
-                    <p className="text-text-primary">
-                        Oh, look! Another bipedal challenger. How... quaint. Welcome, carbon-based life form. Let’s see how you fare in my arena of knowledge. Buckle up.
-                    </p>
-                </BotMessage>
-                <TopicHeader topic="General Knowledge" />
-                <Image src="https://media.springernature.com/lw703/springer-static/image/art%3A10.1038%2F528452a/MediaObjects/41586_2015_Article_BF528452a_Figg_HTML.jpg" alt="chatbot" />
+                <motion.div
+                className="w-full"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.5}}
+                >
+                    <BotMessage>
+                        <p className="text-text-primary">
+                            Oh, look! Another bipedal challenger. How... quaint. Welcome, carbon-based life form. Let’s see how you fare in my arena of knowledge. Buckle up.
+                        </p>
+                    </BotMessage>
+                </motion.div>
+                <motion.div
+                className="w-full flex flex-col items-center gap-4"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 0.5}}
+                >
+                    <TopicHeader topic="General Knowledge" />
+                    <Image src="https://media.springernature.com/lw703/springer-static/image/art%3A10.1038%2F528452a/MediaObjects/41586_2015_Article_BF528452a_Figg_HTML.jpg" alt="chatbot" />
+                </motion.div>
             </div>
             <Footer />
         </div>
