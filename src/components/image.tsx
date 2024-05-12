22844d20af111b433b1b9cb2fdb2cb01aa34e7fc
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-05-12 15:40:07 -0400

    fix: image aspect ratio set to 1:1

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,7 +1,7 @@
 export default function Image({src, alt}: {src: string, alt: string}) {
     return (
         <div className="w-full">
-            <img src={src} alt={alt} className="size-80 m-auto rounded-lg object-cover" />
+            <img src={src} alt={alt} className="w-full aspect-square m-auto rounded-lg object-cover" />
         </div>
     )
 }
