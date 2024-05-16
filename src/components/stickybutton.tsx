c041f4d7d5f7e278b0c74696a934736a58a5ee16
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-05-15 20:19:47 -0400

    fix: z-indexes

--- src/components/stickybutton.tsx
+++ src/components/stickybutton.tsx
@@ -1,6 +1,6 @@
 export default function StickyButton({children}: {children: React.ReactNode}) {
     return (
-        <div className="fixed w-full bottom-0 left-0 z-50">
+        <div className="fixed w-full bottom-0 left-0 z-40">
             <div className="mx-auto flex justify-center px-5 bg-[#F6F9FF]"
             style={{width: "430px"}}
             >
