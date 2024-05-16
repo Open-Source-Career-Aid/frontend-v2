39ac6157c72c3a9ac0131b070b4d923ca4c1a6f5
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-16 12:14:56 -0500

    fix syntax errors

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,4 +1,12 @@
-const Icon = ({ id }: {id: string }, { size = 24, ...props }) => {
+import type { IconName } from './public/icons/name.d.ts';
+
+// extend tells TS we're returning SVGElement
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  id: IconName
+  size?: number
+}
+
+const Icon = ({ id }: {id: string }, { size = 24, ...props }: IconProps) => {
 
   return(
     <svg width={size} height={size} {...props}>
