46d011064613e28c95534017d3f7a595979c07d2
Jonathan Mendler <jon.mendler@gmail.com>
2024-05-17 13:40:28 -0500

    Add explicit type | icon.tsx line 6

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -3,7 +3,7 @@ import { ReactDOM } from "react";
 
 const icons = ['arrow', 'award', 'chatbot_lg', 'chatbot_sm', 'check', 'help', 'lightbulb', 'logo', 'user', 'world', 'x']
 
-export default function Icon({ id, ...props }) {
+export default function Icon({ id }: { id: string }, { ...props }) {
   return (
     <svg {...props}>
       <use href={`/sprite.svg#${id}`} />
