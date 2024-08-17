fb03a7a33a69df0ec3ed97dee58236512ce59c89
lami <olamide2olowoniyi@gmail.com>
2024-08-17 14:59:18 -0400

    fix: removed sqlite db

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-// const API_URL = "http://127.0.0.1:8000"
-const API_URL = "https://apiv2.dumbsplain.com";
+const API_URL = "http://127.0.0.1:8000"
+// const API_URL = "https://apiv2.dumbsplain.com";
 
 export { API_URL }
