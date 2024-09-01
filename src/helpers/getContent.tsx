073dddac3a365447d1eaf2929063b5daaf4a3603
lami <olamide2olowoniyi@gmail.com>
2024-08-31 23:53:03 -0400

    fix: added params to topic GET request so client send its timestamp

--- src/helpers/getContent.tsx
+++ src/helpers/getContent.tsx
@@ -2,8 +2,8 @@ import BaseAPI from "./baseapi";
 import { API_URL } from "../config";
 
 async function getContent() {
-
-	const url = `${API_URL}/topic/`;
+	const timestamp = new Date().getTime(); // get client timestamp
+	const url = `${API_URL}/topic/?q=${timestamp}`;
 	const method = 'GET';
 
 	const response = await BaseAPI({ method, url });
